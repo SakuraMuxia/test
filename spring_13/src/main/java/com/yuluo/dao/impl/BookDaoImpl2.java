7325d7440bdd80bd0c3e6be8321429c957f3d66d
yuluo <2216847528@qq.com>
2022-05-28 17:43:47 +0800

    5月28日提交

--- spring_13/src/main/java/com/yuluo/dao/impl/BookDaoImpl2.java
+++ spring_13/src/main/java/com/yuluo/dao/impl/BookDaoImpl2.java
@@ -3,11 +3,11 @@ package com.yuluo.dao.impl;
 import com.yuluo.dao.BookDao;
 import org.springframework.stereotype.Repository;
 
-@Repository("bookDao1")
-public class BookDaoImpl implements BookDao {
+@Repository("bookDao2")
+public class BookDaoImpl2 implements BookDao {
     @Override
     public void save() {
-        System.out.println("book dao save ...");
+        System.out.println("book dao 2 save ...");
     }
 
 }
--- spring_13/src/main/java/com/yuluo/service/impl/BookServiceImpl.java
+++ spring_13/src/main/java/com/yuluo/service/impl/BookServiceImpl.java
@@ -2,13 +2,16 @@ package com.yuluo.service.impl;
 
 import com.yuluo.dao.BookDao;
 import com.yuluo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    @Qualifier("bookDao1")
     private BookDao bookDao;
 
-    public void setBookDao(BookDao bookDao) {
-        this.bookDao = bookDao;
-    }
 
     @Override
     public void save() {
