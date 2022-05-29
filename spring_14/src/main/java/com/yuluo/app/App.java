ad11e420f4a8d7b69ca6ccbcb14047fdb32f33ac
yuluo <2216847528@qq.com>
2022-05-29 11:34:09 +0800

    5月29日第2次提交

--- spring_14/src/main/java/com/yuluo/app/App.java
+++ spring_14/src/main/java/com/yuluo/app/App.java
@@ -1,11 +1,7 @@
 package com.yuluo.app;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.yuluo.config.SpringConfig;
-import com.yuluo.dao.BookDao;
-import com.yuluo.service.BookService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import javax.sql.DataSource;
 
 public class App {
