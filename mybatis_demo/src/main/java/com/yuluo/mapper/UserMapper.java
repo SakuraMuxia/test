3334a58f5abe6509895190d1b6f0b37d2df8161c
yuluo <2216847528@qq.com>
2022-05-29 17:03:23 +0800

    5月29日第3次提交

--- mybatis_demo/src/main/java/com/yuluo/mapper/UserMapper.java
+++ mybatis_demo/src/main/java/com/yuluo/mapper/UserMapper.java
@@ -8,4 +8,6 @@ public interface UserMapper {
     //定义一个方法
     List<User> selectAll();
 
+    User selectById(int i);
+
 }
---
+++ mybatis_demo/src/test/java/com/yuluo/test/MyBatisTest.java
@@ -0,0 +1,5 @@
+package com.yuluo.test;
+
+public class MyBatisTest {
+
+}
